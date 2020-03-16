2e078cd006ad220d172f747ef71ffa093d78fa79
YASSIR ALBERTO MEZA DIAZ <yasalberto@hotmail.com>
2020-03-15 23:49:42 -0600

    AuthController

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Modelos;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [ActionName("login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromForm] Login model)
+        {
+
+            if (model.UsuarioId == "usuario" && model.Contrasenia == "1234")
+            {
+                model.Nombre = "Hola Mundo";
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier, model.UsuarioId.ToString()),
+                    new Claim(ClaimTypes.Name, model.Nombre)
+                }),
+                    Expires = DateTime.Now.AddDays(7),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes("H0l4MunDO-CoronaV1RuS")),
+                        SecurityAlgorithms.HmacSha512Signature),
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new
+                {
+                    token = tokenHandler.WriteToken(token),
+                    expiration = token.ValidTo 
+                });
+            }
+            return Unauthorized();
+        }
+
+    }
+}
---
+++ WebApi/Controllers/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Modelos
+{
+    public class Login
+    {
+        public string UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
